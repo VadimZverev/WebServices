c48e835b1ad295f7580ea22f2e79638b508726df
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-09-12 16:33:10 +0400

    Создание контроллера Web API

---
+++ WebServices/Controllers/ReservationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class ReservationController : ApiController
+    {
+        private ReservationRespository repo = ReservationRespository.Current;
+
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repo.GetAll();
+        }
+
+        public Reservation GetReservation(int id)
+        {
+            return repo.Get(id);
+        }
+
+        public bool PutReservation(Reservation item)
+        {
+            return repo.Update(item);
+        }
+
+        public void DeleteREservation(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
