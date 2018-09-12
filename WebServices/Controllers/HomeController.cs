a9deeb2186dbc87c566808262a9e6026bd33a2a5
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-09-12 16:11:34 +0400

    Подготовка проекта для главы

---
+++ WebServices/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ReservationRespository repo = ReservationRespository.Current;
+
+        public ViewResult Index()
+        {
+            return View(repo.GetAll());
+        }
+
+        public ActionResult Add(Reservation item)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.Add(item);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
